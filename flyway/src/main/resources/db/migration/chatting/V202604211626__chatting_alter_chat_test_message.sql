e69d260239bbe9b3174d14f9c3cfab7e824cd209
brunosong <playprogramer@gmail.com>
2026-04-22 14:45:21 +0900

    liquibase 기본셋팅

---
+++ flyway/src/main/resources/db/migration/chatting/V202604211626__chatting_alter_chat_test_message.sql
@@ -0,0 +1,6 @@
+-- ============================================================
+-- Domain : chatting
+-- Author : brunosong
+-- Description : 채팅 테이블 컬럼 추가
+-- ============================================================
+ALTER TABLE chat_message ADD COLUMN age varchar(40);
